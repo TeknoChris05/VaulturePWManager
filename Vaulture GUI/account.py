4f0c469139fb887b36aef5e02007f000fa1369b1
TeknoChris05 <50220584+TeknoChris05@users.noreply.github.com>
2025-03-03 13:56:48 -0500

    Made GUI work

--- Vaulture GUI/account.py
+++ Vaulture GUI/account.py
@@ -5,16 +5,26 @@ class Account_Frame(customtkinter.CTkFrame):
     def __init__(self, master):
         super().__init__(master)
 
-        # Retrieve screen width and height
-        screen_dimension_width = self.winfo_screenwidth()
-        screen_dimension_height = self.winfo_screenheight()
+        screen_dimension_width = master.winfo_screenwidth()
+        screen_dimension_height = master.winfo_screenheight()
 
-        self.configure(width= screen_dimension_width/2,
-                        height= screen_dimension_height / 1.5,
-                        corner_radius=15,
-                        bg_color="yellow")
+        self.configure(width=screen_dimension_width / 2,
+                       height=screen_dimension_height / 1.5,
+                       corner_radius=15,
+                       fg_color="gray")
 
         self.pack_propagate(False)
 
         self.label = customtkinter.CTkLabel(self, text="Account Frame", text_color="white", font=("Arial", 20))
         self.label.pack(pady=20)
+
+        self.pack(pady=20)
+
+
+root = customtkinter.CTk()
+root.geometry("800x600")
+
+frame = Account_Frame(root)
+frame.pack(expand=True)
+
+root.mainloop()
